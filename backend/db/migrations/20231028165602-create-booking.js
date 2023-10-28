cef169395ca817b6e19b1f8ae3cc8c5f68e3da9c
Azadcelik <azadcelikk12@gmail.com>
2023-10-28 15:41:38 -0400

    refactored-addIndex

--- backend/db/migrations/20231028165602-create-booking.js
+++ backend/db/migrations/20231028165602-create-booking.js
@@ -55,10 +55,12 @@ module.exports = {
       }
     },options);
 
-    
+    await queryInterface.addIndex('Bookings', ['spotId', 'startDate', 'endDate'], {
+      unique: true
+    });
   },
   async down(queryInterface, Sequelize) {
- 
+    await queryInterface.removeIndex('Bookings', ['spotId', 'startDate', 'endDate']);
     options.tableName = "Bookings";
     return queryInterface.dropTable(options);
   }
