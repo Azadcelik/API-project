946744549244a7a22dfb5fe950936d84e7d22fdc
Azadcelik <azadcelikk12@gmail.com>
2023-10-29 18:44:36 -0400

    changed-seeder-to-check-deploy

--- backend/db/seeders/20231025232603-demo-user.js
+++ backend/db/seeders/20231025232603-demo-user.js
@@ -56,7 +56,7 @@ module.exports = {
         email: 'user4@user.io',
         username: 'FakeUser4',
         hashedPassword: bcrypt.hashSync('password5'),
-        firstName : 'Sergio',
+        firstName : 'Sergioo',
         lastName : 'Ramos'
       }
     ], { validate: true }).catch(err => {
