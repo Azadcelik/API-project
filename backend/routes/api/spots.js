9969309d0ad5f9eda1dea33730ae3bacc2c9de7b
Azadcelik <azadcelikk12@gmail.com>
2023-10-31 22:37:57 -0400

    created-spot-and-spotImage-endpoin

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcryptjs');
 
 
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
-const { Spot,Review,ReviewImage } = require('../../db/models')
+const { Spot,Review,ReviewImage,SpotImage, User } = require('../../db/models')
 
 const router = express.Router();
 
@@ -105,9 +105,113 @@ router.get('/current', async (req,res) => {
 })
 
 
+//first get spot id from endpoint params 
+//then find spots by spotid typically findbypk and include spotImages and owner 
+// but you need to alias user as a owner 
+//add your avgRating but with number of reviews this time.You probably need to just 
+//the total of reviews by length of the review table.you need to add spotImages table
+// and it is all columns as well. double check upperCase and s at the end 
+//you need to add user as well But need to change user table to the owner and include
+//just first and last name 
+
+
+router.get('/:spotId', async (req,res) => { 
+   const {spotId }= req.params
+ 
+   const spots = await Spot.findByPk(spotId, { 
+      include : [
+         {
+            model : SpotImage,
+            attributes : { 
+               exclude : [ "createdAt","updatedAt", 'spotId']
+            }
+         },
+         {
+            model : User,
+            as : 'Owner',
+            attributes : ['id', 'firstName', 'lastName']
+         },
+         {
+            model : Review
+         }
+      ]
+   })
+   if (!spots) { 
+    return  res.status(404).json({message: "Spot couldn't be found"})
+   }
+   
+   const totalStars = spots.Reviews.reduce((acc,review) => acc + review.stars,0)
+   const avgRating = totalStars / spots.Reviews.length
+   
+   const spotData = spots.toJSON()
+
+   spotData.avgRating = avgRating
+   spotData.numReviews = spots.Reviews.length
+
+   delete spotData.Reviews
+
+   res.json(spotData)
+})
 
 
 
+//require authentication before you create your data
+//query from body and retreieve data you need 
+//use create method with key and  value pairs 
+// if any of required details missed return error with the message and errors objects 
+// specify each error but ask where to specify to instructor later 
+
+
+
+router.post('/', async (req,res) => { 
+    const {address,city,state,country,lat,lng,name,description,price} = req.body;
+
+    const spots = await Spot.create({ 
+      ownerId: req.user.id,
+      address,
+      city,
+      state,
+      country,
+      lat,
+      lng,
+      name,
+      description,
+      price
+    })
+    if (!spots) { 
+      res.status(400).json({message: "Bad Request"})
+    }
+    res.status(201).json({spots})
+
+})
+
+
+
+
+router.post('/:spotId/images', async (req,res) => { 
+   const {url,preview} = req.body
+   const {spotId} = req.params
+  
+   const spots = await Spot.findByPk(spotId)
+
+   if (!spots) {
+     return res.status(404).json({message : "Spot couldn't be found"})
+   }
+   
+   const image = await SpotImage.create({ 
+      spotId,
+      url,
+      preview
+   })
+  
+   res.json(
+    {
+      id: image.id,
+      url : image.url,
+      preview : image.preview
+    })
+      
+})
 
 
 
